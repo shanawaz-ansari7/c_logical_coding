a0c42937c86457620ccaceb76a953cdff7ed9e99
shanawaz-ansari7 <shanawaz.ansari9380@gmail.com>
2026-08-04 19:54:27 +0530

    pattern example 13

---
+++ 13.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+
+for(int i=1;i<=4;i++){
+         char ch='A';
+	for(int j=1;j<=4;j++){
+	if(j<=5-i)
+		pritnf("%c",ch);
+	else
+		printf(" ");
+	ch=ch+1;	
+}
+printf("\n")
+}
+return 0;
+}
