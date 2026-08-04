80c38361ce76af3ff12b20239f6a73a9585dce54
shanawaz-ansari7 <shanawaz.ansari9380@gmail.com>
2026-08-04 19:42:38 +0530

    pattern example 10

---
+++ 10.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+
+for(int i=1;i<=4;i++){
+         int num=1;
+	for(int j=1;j<=4;j++){
+	if(j<=5-i)
+		pritnf("%d"num);
+	else
+		printf(" ");
+	num--;	
+}
+printf("\n")
+}
+return 0;
+}
