e96268cca373852c324161f00cc1ee6e668c822c
shanawaz-ansari7 <shanawaz.ansari9380@gmail.com>
2026-08-04 19:10:51 +0530

    pattern example 1

---
+++ 1.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+{
+int n=4;
+
+for(int i=1;i<=n;i++){
+     int num=1;
+	for(int x=1;x<=n;x++){
+		printf("%d",num);
+                 num++;
+	}
+
+printf("\n");
+}
+
+
+return 0;
+}
+
