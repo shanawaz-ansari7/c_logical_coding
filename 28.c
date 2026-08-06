d5c987feb153b1706404f395c4ff638b25e028ec
shanawaz-ansari7 <shanawaz.ansari9380@gmail.com>
2026-08-06 23:58:30 +0530

    pattern example 29

---
+++ 28.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main() 
+{
+int i,j;
+	for(i=1;i<=9;i++)
+	{
+		for(j=1;j<=9;j++)
+		{
+			if(j<=10-i&&j>=0+i)
+				printf("*");
+			
+			else
+				printf(" ");
+		}
+		printf("\n");	
+	}
+return 0;
+}
