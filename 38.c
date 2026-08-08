a5cd56a53a5e0d48fea5e305ab1f46733af9a8c2
shanawaz-ansari7 <shanawaz.ansari9380@gmail.com>
2026-08-08 15:11:45 +0530

    pattern example 38

---
+++ 38.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main()
+{
+int i,j;
+for(i=1;i<=5;i++)
+{
+	int num=1;
+	for(j=1;j<=5;j++)
+	{
+		if(i==1||i==5||j==1||j==5)
+			printf("%d",num);
+		else
+			printf(" ");
+				num++;
+			
+			}
+	
+printf("\n");
+}
+return 0;
+}
