e41ede1550409e3a1c9061a76531987c635fb9d4
shanawaz-ansari7 <shanawaz.ansari9380@gmail.com>
2026-08-08 15:30:37 +0530

    pattern example 42

---
+++ 42.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main()
+{
+int i,j;
+for(i=1;i<=5;i++)
+{
+	char ch='A';
+	for(j=1;j<=5;j++)
+	{
+		if(i==1||i==5||j==1||j==5)
+			printf("%c",ch);
+		else
+			printf(" ");
+				ch=ch+1;
+			
+			}
+	
+printf("\n");
+}
+return 0;
+}
