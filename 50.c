11915326a0370e9b9c8dab9eca2786ed67ef3629
shanawaz-ansari7 <shanawaz.ansari9380@gmail.com>
2026-08-10 18:58:29 +0530

    pattern example 50

---
+++ 50.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main()
+{
+int i,j;
+for(i=1;i<=5;i++)
+{
+	int num=1;
+	for(j=1;j<=9;j++)
+	{
+		if(j<=6-i||j>=4+i)
+			printf("%d",num);
+		else
+			printf(" ");
+			num++;
+			
+			}
+	
+printf("\n");
+}
+return 0;
+}
