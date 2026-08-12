1005d69e6f9a0c877bd71b157fe75deab6e9b5db
shanawaz-ansari7 <shanawaz.ansari9380@gmail.com>
2026-08-12 13:51:08 +0530

    pattern example 61

--- 61.c
+++ 61.c
@@ -5,14 +5,14 @@ int main()
 int i,j;
 for(i=1;i<=5;i++)
 {
-	char ch='a';
+	char ch='E';
 	for(j=1;j<=5;j++)
 	{
 		if(i==3||j==5)
 			printf("%c ",ch); 	
 		else
 			printf(" ");
-			ch=ch+1;			
+			ch=ch-1;			
 			}
 			
 printf("\n");
