735ee78f66362d8ddfc595fec418238fca743360
shanawaz-ansari7 <shanawaz.ansari9380@gmail.com>
2026-08-14 20:51:44 +0530

    pattern example 78

---
+++ 78.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main()
+{
+int i,j;
+for(i=1;i<=11;i++)
+{
+	char ch='a';
+		for(j=1;j<=11;j++)
+	{
+		if(j>=i||j>=12-i)
+			printf("%c",ch); 	
+		else
+			printf(" ");
+			ch=ch+1;		
+			}
+			
+printf("\n");
+}
+return 0;
+}
