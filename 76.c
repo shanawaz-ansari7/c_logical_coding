76a6e576e065dc0202aa10bb36b4dbe13bcd63ea
shanawaz-ansari7 <shanawaz.ansari9380@gmail.com>
2026-08-14 20:45:12 +0530

    pattern example 76

---
+++ 76.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main()
+{
+int i,j;
+for(i=1;i<=11;i++)
+{
+	int num=1;
+		for(j=1;j<=11;j++)
+	{
+		if(j>=i||j>=12-i)
+			printf("%d",num); 	
+		else
+			printf(" ");
+			num++;		
+			}
+			
+printf("\n");
+}
+return 0;
+}
