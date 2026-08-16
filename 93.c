dd0ffe51f2bf5edd59b1b8ea8689ee2dda6c372b
shanawaz-ansari7 <shanawaz.ansari9380@gmail.com>
2026-08-16 22:47:42 +0530

    pattern example 93

---
+++ 93.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main()
+{
+int i,j,n;
+printf("Entern a number :");
+scanf("%d",&n);
+for(i=1;i<=n;i++)
+
+{
+	char ch='a';
+		for(j=1;j<=n;j++)
+	{
+		if(j==i||j==n+1-i)
+			printf("%c",ch); 	
+		else
+			printf(" ");
+			ch=ch+1;		
+			}
+			
+printf("\n");
+}
+return 0;
+}
