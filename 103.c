de0ca5e1b7bf1892f89846f3713eec467d21cc5d
shanawaz-ansari7 <shanawaz.ansari9380@gmail.com>
2026-08-18 20:13:42 +0530

    pattern example 103

---
+++ 103.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main()
+{
+int i,j,k=0;
+
+for(i=1;i<=9;i++)
+{
+	char ch='a';
+	i<=5 ? k++ : k--;
+		for(j=1;j<=9;j++)
+	{
+		if(j<=6-k||j>=4+k)
+			printf("%c",ch); 	
+		else
+			printf(" ");
+			ch=ch+1;		
+			}
+			
+printf("\n");
+}
+return 0;
+}
