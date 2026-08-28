778c21072bfa7d83eac049d8bec25b7c1039557e
shanawaz-ansari7 <shanawaz.ansari9380@gmail.com>
2026-08-28 21:04:36 +0530

    pattern example 162

---
+++ 162.c
@@ -0,0 +1,32 @@
+
+#include <stdio.h>
+
+int main() {
+    int n = 5;
+    int i, j;
+   char ch='a';
+
+    for (i = n; i >= 1; i--) {
+        for (j = 1; j <= n - i; j++)
+            printf(" ");
+
+        for (j = 1; j <= 2 * i - 1; j++)
+            printf("%d",num);
+
+        printf("\n");	    }
+
+
+    for (i = 2; i <= n; i++) {
+        for (j = 1; j <= n - i; j++)
+            printf(" ");
+
+        for (j = 1; j <= 2 * i - 1; j++)
+             printf("%d",num);
+
+        printf("\n");
+
+    }
+
+    return 0;
+}
+
