eb4f970f6906be1af08779fff419b5a8691f7f6e
shanawaz-ansari7 <shanawaz.ansari9380@gmail.com>
2026-09-07 22:33:32 +0530

    pattenr example 221

---
+++ 221.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    int i, j, n = 5;
+    int num;
+
+    for(i = 0; i < n; i++)
+    {
+        num = 1;
+
+        for(j = 0; j < n-i; j++)
+            printf(" ");
+
+        for(j = 0; j <= i; j++)
+        {
+            printf("%d ", num);
+            num = num * (i-j) / (j+1);
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
