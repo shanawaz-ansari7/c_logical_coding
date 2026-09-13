e4e23947bb48a662f8647d256d2fd11eccd2facb
shanawaz-ansari7 <shanawaz.ansari9380@gmail.com>
2026-09-13 23:02:50 +0530

    array  example 255

---
+++ 255.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    int x[]={1,2,3,5,61,3,19,7,22,9,80};
+    int n=sizeof(x)/sizeof(x[0]), count=0;
+    for(int i=0;i<n;i++) if(x[i]>=10) count++;
+    int y[count], j=0;
+    for(int i=0;i<n;i++) if(x[i]>=10) y[j++]=x[i];
+    for(int i=0;i<count;i++) printf("%d ",y[i]);
+    return 0;
+}
