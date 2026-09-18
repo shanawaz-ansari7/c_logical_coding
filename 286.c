f83e2d89a990ee75dbcbc163d42ca67991e1b759
shanawaz-ansari7 <shanawaz.ansari9380@gmail.com>
2026-09-18 23:43:43 +0530

    array in cpp

---
+++ 286.c
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int x[]={1,2,1,5,6,5,7,4,8,9,8};
+    int n=sizeof(x)/sizeof(x[0]);
+    for(int i=1;i<n-1;i++)
+        if(x[i-1]==x[i+1]) cout << i << ",";
+    return 0;
+}
